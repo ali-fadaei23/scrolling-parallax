57d943ef152897658046ad8b8e9b1bbf44309f26
alifadaei <razorn1375@gmail.com>
2023-11-06 14:21:57 +0330

    handle change section  with animation

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -18,14 +18,64 @@ const gsapInit = () => {
 
   gsap.set(outerWrappers, { yPercent: 100 });
   gsap.set(innerWrappers, { yPercent: -100 });
+
+  const gotoSection = (index, direction) => {
+    index = wrap(index);
+    animating = true;
+    let fromTop = direction === -1,
+      dFactor = fromTop ? -1 : 1,
+      tl = gsap.timeline({
+        defaults: { duration: 1.25, ease: "power1.inOut" },
+        onComplete: () => (animating = false),
+      });
+
+    if (currentIndex >= 0) {
+      gsap.set(sections[currentIndex], { zIndex: 0 });
+      tl.to(images[currentIndex], { yPercent: -15 * dFactor }).set(
+        sections[currentIndex],
+        { autoAlpha: 0 }
+      );
+    }
+    gsap.set(sections[index], { autoAlpha: 1, zIndex: 1 });
+    tl.fromTo(
+      [outerWrappers[index], innerWrappers[index]],
+      {
+        yPercent: (i) => (i ? -100 * dFactor : 100 * dFactor),
+      },
+      { yPercent: 0 },
+      0
+    ).fromTo(images[index], { yPercent: 15 * dFactor }, { yPercent: 0 }, 0);
+    currentIndex = index;
+  };
 };
+
+const sections = [
+  { title: "Scrolling", image: image1 },
+  { title: "Infinite", image: image2 },
+  { title: "Website", image: image3 },
+];
 
 function Parallax() {
   useEffect(() => {
     gsap.registerPlugin(Observer);
     gsapInit();
   }, []);
-  return <></>;
+  return sections.map((section) => {
+    return (
+      <section key={section.title}>
+        <div className="outer">
+          <div className="inner">
+            <div
+              className="bg"
+              style={{ backgroundImage: `url(${section.image})` }}
+            >
+              <h2>{section.title}</h2>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  });
 }
 
 export default Parallax;
