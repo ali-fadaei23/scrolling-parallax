769a2f98a0684e1660e1dd0701c66e16a2e1cb2d
alifadaei <razorn1375@gmail.com>
2023-11-06 13:47:56 +0330

    Import images

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,4 +1,11 @@
 import "./Parallax.css";
+import { useEffect } from "react";
+import gsap from "gsap";
+import { Observer } from "gsap/all";
+//########## Add Images ##########
+import image1 from "../../assets/images/1.jpg";
+import image2 from "../../assets/images/2.jpg";
+import image3 from "../../assets/images/3.jpg";
 
 function Parallax() {
   return <></>;
